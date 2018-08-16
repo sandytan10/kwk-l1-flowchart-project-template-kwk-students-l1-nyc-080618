6441c096a17bf5a79b111279cd56042a9b33af7b
sandytan10 <stan10@stuy.edu>
2018-08-16 18:40:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,78 @@
+
 class ApplicationController < Sinatra::Base
-
+  
   configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+    set :public_folder, "public"
+    set :views, "app/views"
+  end 
+  
+  get '/' do
+    erb :front_page
   end
+  
 
+post  "/begin" do 
+    erb :begin
 end
+
+  post  "/a" do 
+    erb :a
+end
+
+post  "/b" do 
+    erb :b
+end
+
+post  "/aa" do 
+  erb :aa
+end
+
+post "/ab" do
+  erb :ab
+end
+
+post "/aaa" do
+  erb :aaa
+end
+
+post "/aaa_end" do
+  erb :aaa_end
+end
+
+post "/aab" do
+  erb :aab
+end
+
+post "/aab_end" do
+  erb :aab_end
+end
+
+post  "/one_b" do 
+    erb :one_b
+end
+
+post  "/ab" do 
+    erb :ab
+end
+
+post  "/aba" do 
+    erb :aba
+end
+
+post  "/abaa" do 
+    erb :abaa
+end
+
+post  "/abab" do 
+    erb :abab
+end
+
+post  "/abb" do 
+    erb :abb
+end
+
+
+end 
+
+
+
